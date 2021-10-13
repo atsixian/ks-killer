b88776a0e930f29aa5827cffcf7c7bfdbb5bed41
Sixian Li <sixianli@microsoft.com>
2021-10-13 13:30:33 +0800

    Add jest

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+/*
+ * @copyright Microsoft Corporation. All rights reserved.
+ */
+module.exports = {
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript']
+};
