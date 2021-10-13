6bc61c586ecff0ed997acb735806f087e2c9d754
Sixian Li <sixianli@microsoft.com>
2021-10-13 14:31:49 +0800

    Remove binExp test for now

--- src/elimination/binaryExp.test.ts
+++ src/elimination/binaryExp.test.ts
@@ -1,17 +1,3 @@
 /*
  * @copyright Microsoft Corporation. All rights reserved.
  */
-
-import { Project, SyntaxKind } from 'ts-morph';
-import { handleBinaryExp } from './binaryExp';
-
-const project = new Project({
-  tsConfigFilePath: './tsconfig.json'
-});
-
-const srcFile = project.getSourceFile('src/test/binaryExp.ts');
-const binaryExps = srcFile.getDescendantsOfKind(SyntaxKind.BinaryExpression);
-
-binaryExps.forEach(handleBinaryExp);
-
-console.log(srcFile.print());
