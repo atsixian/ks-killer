3ba430e091db8bba2d50f0f7978e3df97dcb691d
Sixian Li <sixianli@microsoft.com>
2021-10-14 16:31:41 +0800

    Handle conditionals

---
+++ src/optimization/conditionalExp.test.ts
@@ -0,0 +1,57 @@
+/*
+ * @copyright Microsoft Corporation. All rights reserved.
+ */
+
+import { SourceFile, SyntaxKind } from 'ts-morph';
+import { optimize } from '.';
+import { getInfoFromText } from '../utils';
+import { handleConditionalExp } from './conditionalExp';
+
+describe('Conditional Expression', () => {
+  describe('Simple conditional', () => {
+    it('should remove else branch when cond is always true', () => {
+      const { sourceFile } = getInfoFromText(`
+        true ? A() : B
+    `);
+      handleCondExps(sourceFile);
+      expect(sourceFile.getText().trim()).toBe(`A()`);
+    });
+    it('should remove then branch when cond is always false', () => {
+      const { sourceFile } = getInfoFromText(`
+        false ? A() : B
+    `);
+      handleCondExps(sourceFile);
+      expect(sourceFile.getText().trim()).toBe(`B`);
+    });
+  });
+
+  describe('Nested conditional', () => {
+    it('should handle nested conditionals', () => {
+      const { sourceFile } = getInfoFromText(`
+        (true ? A() : B()) ? C() : (false ? D() : E())
+      `);
+      handleCondExps(sourceFile, true);
+      expect(sourceFile.getText().trim()).toBe(`A() ? C() : E()`);
+    });
+    it('should handle && expression parent', () => {
+      const { sourceFile } = getInfoFromText(`
+        (true ? true : B()) || C     
+      `);
+      handleCondExps(sourceFile, true);
+      expect(sourceFile.getText().trim()).toBe(`true`);
+    });
+    it('should handle || expression parent', () => {
+      const { sourceFile } = getInfoFromText(`
+        (true ? true : B()) && C     
+      `);
+      handleCondExps(sourceFile, true);
+      expect(sourceFile.getText().trim()).toBe(`C`);
+    });
+  });
+});
+
+function handleCondExps(sourceFile: SourceFile, nested: boolean = false) {
+  sourceFile
+    .getDescendantsOfKind(SyntaxKind.ConditionalExpression)
+    .forEach(nested ? optimize : handleConditionalExp);
+}
---
+++ src/optimization/conditionalExp.ts
@@ -0,0 +1,17 @@
+/*
+ * @copyright Microsoft Corporation. All rights reserved.
+ */
+import { ConditionalExpression, SyntaxKind, Node, ts } from 'ts-morph';
+import { HandlerReturnType } from '.';
+import { tryUnwrapParenthese } from '../utils';
+
+export function handleConditionalExp(exp: ConditionalExpression): HandlerReturnType {
+  const cond = exp.getCondition();
+  let newWork: HandlerReturnType;
+  if (cond.getKind() === SyntaxKind.TrueKeyword) {
+    newWork = exp.replaceWithText(exp.getWhenTrue().getText());
+  } else if (cond.getKind() === SyntaxKind.FalseKeyword) {
+    newWork = exp.replaceWithText(exp.getWhenFalse().getText());
+  }
+  return tryUnwrapParenthese(newWork);
+}
