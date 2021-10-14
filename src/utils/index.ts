1723a3805f3836271af822ef472578a95743b47e
Sixian Li <sixianli@microsoft.com>
2021-10-14 10:34:13 +0800

    Add print reference util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@
  * @copyright Microsoft Corporation. All rights reserved.
  */
 
-import { Block, Node, Statement, ts } from 'ts-morph';
+import { Block, Node, ReferenceEntry, Statement, ts } from 'ts-morph';
 
 /**
  * Determine if nodeA is an ancestor of nodeB
@@ -14,6 +14,17 @@ export function isAncestorOf(nodeA: Node<ts.Node>, nodeB: Node<ts.Node>): boolea
 export function unwrapBlock(target: Statement | Block): string {
   // https://github.com/dsherret/ts-morph/issues/641
   return target.getChildSyntaxListOrThrow().getText({ trimLeadingIndentation: true });
+}
+
+export function printReference(reference: ReferenceEntry) {
+  console.log('---------');
+  console.log('REFERENCE');
+  console.log('---------');
+  console.log(`File path:  ${reference.getSourceFile().getFilePath()}`);
+  console.log(`Start: ${reference.getTextSpan().getStart()}`);
+  console.log(`Length: ${reference.getTextSpan().getLength()}`);
+  console.log(`Parent kind: ${reference.getNode().getParentOrThrow().getKindName()}`);
+  console.log('\n');
 }
 
 export * from './getInfoFromText';
