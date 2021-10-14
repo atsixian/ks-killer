573e7cc97c1488bae7442b8aeae9fc9ffa1fd719
Sixian Li <sixianli@microsoft.com>
2021-10-14 18:38:54 +0800

    Fix link

--- src/cli.ts
+++ src/cli.ts
@@ -5,16 +5,19 @@ import yargs from 'yargs/yargs';
 
 const argv = yargs(process.argv.slice(2))
   .command(
-    '$0',
+    '$0 <ID>',
     'KS Killer yo',
     (yargs) => {
-      yargs.options({
-        projectDirectory: { type: 'string', default: process.cwd() },
-        ksFilePath: { type: 'string' }
-      });
+      yargs
+        .positional('ID', {
+          describe: 'ID of the KS to graduate',
+          type: 'string'
+        })
+        .options({
+          projectDirectory: { type: 'string', default: process.cwd() },
+          ksFilePath: { type: 'string' }
+        });
     },
-    () => {
-      console.log('hi');
-    }
+    ({ ID: id }) => console.log(id)
   )
   .help().argv;
