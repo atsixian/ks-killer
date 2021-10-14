a2bd31ab7fa47673eb2231da4bbdd449341fd06e
Sixian Li <sixianli@microsoft.com>
2021-10-14 16:48:32 +0800

    Finish optimize

---
+++ src/optimization/optimize.test.ts
@@ -0,0 +1,32 @@
+/*
+ * @copyright Microsoft Corporation. All rights reserved.
+ */
+
+import { SourceFile, SyntaxKind } from 'ts-morph';
+import { optimize } from '.';
+import { getInfoFromText } from '../utils';
+
+describe('Optimize', () => {
+  it('should remove simple if with nested binary', () => {
+    const { sourceFile } = getInfoFromText(`
+       if ((true || A) && (true || B())) {
+         s = 1;
+       } 
+    `);
+    handleOptimize(sourceFile, SyntaxKind.IfStatement);
+    expect(sourceFile.getText().trim()).toBe(`s = 1;`);
+  });
+  it('should remove simple if with nested conditional', () => {
+    const { sourceFile } = getInfoFromText(`
+       if ((false ? T() : (false && A())) ? T() : (true || A)) {
+         s = 1;
+       } 
+    `);
+    handleOptimize(sourceFile, SyntaxKind.IfStatement);
+    expect(sourceFile.getText().trim()).toBe(`s = 1;`);
+  });
+});
+
+function handleOptimize(sourceFile: SourceFile, kind: SyntaxKind) {
+  sourceFile.getDescendantsOfKind(kind).forEach(optimize);
+}
---
+++ src/optimization/optimize.ts
@@ -0,0 +1,32 @@
+/*
+ * @copyright Microsoft Corporation. All rights reserved.
+ */
+
+import { SyntaxKind, ts } from '@ts-morph/common';
+import { BinaryExpression, ConditionalExpression, IfStatement, Node } from 'ts-morph';
+
+import { handleBinaryExp } from './binaryExp';
+import { handleConditionalExp } from './conditionalExp';
+import { handleIf } from './if';
+
+export type HandlerReturnType = Node<ts.Node> | undefined;
+
+export function optimize(node: Node<ts.Node>) {
+  if (!node || node.wasForgotten()) return;
+  let newWork: HandlerReturnType;
+  switch (node.getKind()) {
+    case SyntaxKind.BinaryExpression:
+      newWork = handleBinaryExp(node as BinaryExpression);
+      break;
+    case SyntaxKind.ConditionalExpression:
+      newWork = handleConditionalExp(node as ConditionalExpression);
+      break;
+    case SyntaxKind.IfStatement:
+      handleIf(node as IfStatement);
+      break;
+  }
+  // if there's any optimization happened
+  if (newWork) {
+    optimize(newWork.getParent());
+  }
+}
