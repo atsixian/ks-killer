deb6ff77a671f6d761338df28253170c15277e4b
Sixian Li <sixianli@microsoft.com>
2021-10-15 11:07:31 +0800

    Set up CLI

--- src/cli.ts
+++ src/cli.ts
@@ -4,20 +4,29 @@
 import yargs from 'yargs/yargs';
 
 const argv = yargs(process.argv.slice(2))
+  .usage('ks-killer <KS ID> [options...]')
+  .showHelpOnFail(true, 'Please provide a KS ID')
   .command(
     '$0 <ID>',
-    'KS Killer yo',
+    'KS Killer',
     (yargs) => {
       yargs
         .positional('ID', {
           describe: 'ID of the KS to graduate',
           type: 'string'
         })
-        .options({
-          projectDirectory: { type: 'string', default: process.cwd() },
-          ksFilePath: { type: 'string' }
+        .option('project-path', {
+          alias: 'p',
+          type: 'string',
+          describe: 'Target project to clean. Default to cwd.',
+          default: process.cwd()
+        })
+        .option('ks-file-path', {
+          alias: 'k',
+          type: 'string',
+          describe: 'The file path of the KS. This boosts performance.'
         });
     },
-    ({ ID: id }) => console.log(id)
+    ({ ID: id, projectPath, ksFilePath }) => console.log(id, projectPath, ksFilePath)
   )
   .help().argv;
