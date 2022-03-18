14dd81436e9a06bcd90c6ce6937047d1667c21ce
Sixian Li <lisixian579@gmail.com>
2022-03-18 14:52:34 +0800

    eslint: update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,45 @@
+/** @type {import('eslint').Linter.Config} */
+/** @type {import('@typescript-eslint/utils').TSESLint.Linter.Config} */
 module.exports = {
-  env: {
-    es2021: true,
-    node: true
+  'env': {
+    'es2021': true,
+    'node': true
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
   },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single', { allowTemplateLiterals: true }],
-    semi: ['error', 'always']
+  'plugins': [
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single',
+      { 'allowTemplateLiterals': true }
+    ],
+    'semi': [
+      'error',
+      'always',
+            
+    ],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }
+    ]
   }
 };
