4ee5899c240f5342a76324665088bf090aa3a399
Sixian Li <lisixian579@gmail.com>
2022-03-30 00:03:28 +0800

    fix: remove unused function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,5 @@
 export * from './test-helper/printReference';
 export * from './test-helper/getInfoFromText';
-export * from './isAncestorOf';
 export * from './unwrap';
 export * from './isConstantExpr';
 export * from './dateParser';
